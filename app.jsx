3a80897a15e0a0f9c7025f0525e49d40a3e3fe8c
23TV <30909813+23TV@users.noreply.github.com>
2017-08-10 22:46:09 +0300

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,33 @@
+var jsx = 
+    
+    <div id="container">
+      <h3 className="fa fa-video-camera" aria-hidden="true"> Видеостудия</h3>
+      <h1>#23TV</h1>
+       <hr/>
+       
+      <h4>О нас</h4>
+      <p className="fa fa-video-camera"> Мы молодая школьная видеостудия</p>
+      <p className="fa fa-plus"> Основана в 2016 году</p>
+      <p className="fa fa-map-marker"> Мы находимся в городе Иваново, 23 гимназии</p>
+      <hr/>
+      
+      <h3>Наши видео</h3>
+      
+    <i className="fa fa-file-video-o"> <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/6PZ8r7ky3Rg" frameborder="0" allowfullscreen></iframe></i>
+    <br/>
+    <br/>
+    <i className="fa fa-file-video-o"> <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/byoJGd4U9xg" frameborder="0" allowfullscreen></iframe></i>
+    <br/>
+    <br/>
+    <i className="fa fa-file-video-o"> <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/OF0yS2ryZ9A" frameborder="0" allowfullscreen></iframe></i>
+    <br/>
+    <br/>
+    <i className="fa fa-file-video-o"> <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/fszwqb3-xw0" frameborder="0" allowfullscreen></iframe></i>
+    <hr/>
+    <a href="https://vk.com/mediacentr23"><i className="fa fa-vk"></i></a>
+    <br/>
+    <a href="https://www.youtube.com/channel/UC1BIF_7YXKZzVj3k6Dcb0sA"><i  className="fa fa-youtube-play"></i></a>
+    </div>;
+      
+    
+ReactDOM.render(jsx, document.getElementById('root'));
