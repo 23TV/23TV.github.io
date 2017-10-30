f42980e9fa9088ea248514759bf1dddb7f02be2e
23TV <30909813+23TV@users.noreply.github.com>
2017-10-30 20:14:40 +0300

    Add files via upload

--- app.jsx
+++ app.jsx
@@ -46,7 +46,36 @@ var jsx =
         autoRegister: false,
         notifyButton: {
           enable: true /* Set to false to hide */
+        },
+        {
+    /* Your other init options here */
+    notifyButton: {
+        enable: true, /* Required to use the notify button */
+        size: 'medium', /* One of 'small', 'medium', or 'large' */
+        theme: 'default', /* One of 'default' (red-white) or 'inverse" (white-red) */
+        position: 'bottom-right', /* Either 'bottom-left' or 'bottom-right' */
+        offset: {
+            bottom: '0px',
+            left: '0px', /* Only applied if bottom-left */
+            right: '0px' /* Only applied if bottom-right */
+        },
+        prenotify: true, /* Show an icon with 1 unread message for first-time site visitors */
+        showCredit: false, /* Hide the OneSignal logo */
+        text: {
+            'tip.state.unsubscribed': 'Subscribe to notifications',
+            'tip.state.subscribed': "Вы подписаны",
+            'tip.state.blocked': "You've blocked notifications",
+            'message.prenotify': 'Click to subscribe to notifications',
+            'message.action.subscribed': "Thanks for subscribing!",
+            'message.action.resubscribed': "Вы подписаны",
+            'message.action.unsubscribed': "You won't receive notifications again",
+            'dialog.main.title': 'Manage Site Notifications',
+            'dialog.main.button.subscribe': 'SUBSCRIBE',
+            'dialog.main.button.unsubscribe': 'UNSUBSCRIBE',
+            'dialog.blocked.title': 'Unblock Notifications',
+            'dialog.blocked.message': "Follow these instructions to allow notifications:"
         }
+    }
       }]);
                  
 
